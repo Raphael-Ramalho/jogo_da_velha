1747e397368fd0a0ce875d1bbcd4f0f0dd1493d1
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-17 17:30:25 -0300

    Update index.js

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css"
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
     render() {
         return (
             <button className="square"
-            onClick={() => this.setState({value:"X"})}>
-                {this.state.value}
+            onClick={() => this.props.onClick({value:"X"})}>
+                {this.props.value}
             </button>
         );
     }
@@ -24,6 +18,11 @@ class Board extends React.Component {
         this.state = {
             squares: Array(9).fill(null),
         };
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = "X";
+        this.setState({squares: squares});
     }
     renderSquare(i) {
         return <Square 
